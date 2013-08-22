e8dc66e89b2d01e1c288d428795fb11bfe9a4734
Aaron Zitzer <azitzer@gmail.com>
2013-08-22 15:12:30 -0700

    increase number for pagination

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -5,7 +5,7 @@ class PinsController < ApplicationController
   # GET /pins.json
  
       def index
-    @pins = Pin.order("created_at desc").page(params[:page]).per_page(5)
+    @pins = Pin.order("created_at desc").page(params[:page]).per_page(20)
 
     respond_to do |format|
       format.html # index.html.erb
